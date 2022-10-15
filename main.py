f9d685a44196cfe808936073a50e53e2495420ed
fLUM1NUS <dearva@yandex.ru>
2022-10-16 00:18:24 +0900

    added 1-st lesson added dark style (QDarkStyleSheet by ColinDuquesnoy)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 from PyQt5 import uic, QtGui, QtCore
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, QFile, QTextStream
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
@@ -56,9 +56,85 @@ class LearnWindow(QMainWindow):
         self.setWindowIcon(QtGui.QIcon('res/App_logo-256.png'))
         self.setWindowTitle('Учебник PyQt')
 
+        self.Btn_1.clicked.connect(self.show_text)
+        self.Btn_2.clicked.connect(self.show_text)
+        self.Btn_3.clicked.connect(self.show_text)
+        self.Btn_4.clicked.connect(self.show_text)
+        self.Btn_5.clicked.connect(self.show_text)
+        self.Btn_6.clicked.connect(self.show_text)
+        self.Btn_7.clicked.connect(self.show_text)
+        self.Btn_8.clicked.connect(self.show_text)
+        self.Btn_9.clicked.connect(self.show_text)
+        self.Btn_10.clicked.connect(self.show_text)
+        self.Btn_11.clicked.connect(self.show_text)
+        self.Btn_12.clicked.connect(self.show_text)
+        self.Btn_13.clicked.connect(self.show_text)
+
+    def show_text(self):
+        match self.sender().objectName():
+            case "Btn_1":
+                with open('lessons/lesson_1.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_2":
+                with open('lessons/lesson_2.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_3":
+                with open('lessons/lesson_3.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_4":
+                with open('lessons/lesson_4.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_5":
+                with open('lessons/lesson_5.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_6":
+                with open('lessons/lesson_6.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_7":
+                with open('lessons/lesson_7.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_8":
+                with open('lessons/lesson_8.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_9":
+                with open('lessons/lesson_9.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_10":
+                with open('lessons/lesson_10.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_11":
+                with open('lessons/lesson_11.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_12":
+                with open('lessons/lesson_12.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+            case "Btn_13":
+                with open('lessons/lesson_13.txt', mode='rt', encoding='utf-8') as f:
+                    data = f.read()
+                self.textBrowser.setText(data)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # Подключение стилей {
+    file = QFile("res/darkstyle.qss")
+    file.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(file)
+    app.setStyleSheet(stream.readAll())
+    # Подключение стилей }
     ex = StartWindow()
     ex.show()
     sys.exit(app.exec_())
